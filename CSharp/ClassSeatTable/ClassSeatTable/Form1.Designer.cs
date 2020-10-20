4b8038958506303f0a026adea6890f22be9b78d4
tyrosine1153 <tyrosine1153@gmail.com>
2020-10-20 21:52:56 +0900

    Csharp
    new project

---
+++ CSharp/ClassSeatTable/ClassSeatTable/Form1.Designer.cs
@@ -0,0 +1,104 @@
+﻿using System.Windows.Forms;
+
+namespace ClassSeatTable
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// 필수 디자이너 변수입니다.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// 사용 중인 모든 리소스를 정리합니다.
+		/// </summary>
+		/// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form 디자이너에서 생성한 코드
+
+		/// <summary>
+		/// 디자이너 지원에 필요한 메서드입니다. 
+		/// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.button1 = new System.Windows.Forms.Button();
+			this.SuspendLayout();
+			// 
+			// textBox1
+			// 
+			this.textBox1.Location = new System.Drawing.Point(749, 12);
+			this.textBox1.Name = "textBox1";
+			this.textBox1.Size = new System.Drawing.Size(75, 21);
+			this.textBox1.TabIndex = 0;
+			this.textBox1.Text = "학생 수";
+			this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(749, 39);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(75, 23);
+			this.button1.TabIndex = 1;
+			this.button1.Text = "돌리기";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.button1_Click);
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(836, 497);
+			this.Controls.Add(this.button1);
+			this.Controls.Add(this.textBox1);
+			this.Name = "Form1";
+			this.Text = "Form1";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		private Panel MakePanel(int i) {
+			Panel panel1;
+			panel1 = new System.Windows.Forms.Panel();
+			// 
+			// panel1
+			// 
+			panel1.BackColor = System.Drawing.SystemColors.MenuHighlight;
+			panel1.Location = new System.Drawing.Point(33 + 140 * (i%5), 33 + 80 * (i/5));
+			panel1.Name = "panel1";
+			panel1.Size = new System.Drawing.Size(131, 77);
+			panel1.TabIndex = 1;
+
+			Controls.Add(panel1);
+			return panel1;
+		}
+		private Label MakeLable(int i) {
+			Label label1;
+			label1 = new System.Windows.Forms.Label();
+
+			label1.AutoSize = true;
+			label1.Location = new System.Drawing.Point(33 + 140 * (i % 5), 33 + 80 * (i / 5));
+			label1.Name = "label1";
+			label1.Size = new System.Drawing.Size(38, 12);
+			label1.TabIndex = 0;
+			label1.Text = $"{i+1}";
+
+			Controls.Add(label1);
+			return label1;
+		}
+		#endregion
+
+		private TextBox textBox1;
+		private Button button1;
+	}
+}
---
+++ CSharp/ClassSeatTable/ClassSeatTable/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ClassSeatTable
+{
+	public partial class Form1 : Form
+	{
+		List<Panel> panelList;
+		List<Label> labelList;
+		public Form1()
+		{
+			InitializeComponent();
+			panelList = new List<Panel>();
+			labelList = new List<Label>();
+		}
+		private void button1_Click(object sender, EventArgs e)
+		{
+			if (textBox1.Text.GetType() == typeof(int)) { }
+			int input = Convert.ToInt32(textBox1.Text);
+			MakeSeat(input);
+			ShuppleSeat();
+		}
+		/*
+		 자리수 랜덤하게 섞임. 2중 for 문을 이용해서 서로 바꿔주는 방식 
+		Visible = false, null
+		*/
+		private void ShuppleSeat() {
+			int labelCnt = labelList.Count;
+			for (int i = 0; i < labelCnt; i++) {
+				Random r = new Random(); 
+				int ran = r.Next(1,labelCnt);
+				string temp = this.labelList[i].Text;
+				this.labelList[i].Text = this.labelList[ran].Text;
+				this.labelList[ran].Text = temp;
+			}
+		}
+		private void MakeSeat(int n) {
+			for (int i = 0; i < n; i++)
+			{
+				labelList.Add(MakeLable(i));
+				panelList.Add(MakePanel(i));
+			}
+		}
+	}
+}
