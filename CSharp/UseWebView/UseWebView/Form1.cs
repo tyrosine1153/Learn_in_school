3740582ca559a712a4251ca174e1eec923ec56d8
tyrosine1153 <tyrosine1153@gmail.com>
2020-10-27 17:20:11 +0900

    Csharp_new_solution

---
+++ CSharp/UseWebView/UseWebView/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UseWebView
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private void webBrowser1_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
+		{
+			if (webBrowser1.Url.AbsoluteUri != e.Url.AbsoluteUri) return;
+			int asdf = 0;
+			asdf++;
+			//webBrowser1.DocumentText
+		}
+	}
+}
