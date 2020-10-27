00ae03f28629868d6a6b119881a97d6d80525829
tyrosine1153 <tyrosine1153@gmail.com>
2020-10-27 20:21:41 +0900

    network_asdf

--- CSharp/UseWebView/UseWebView/Form1.Designer.cs
+++ CSharp/UseWebView/UseWebView/Form1.Designer.cs
@@ -29,6 +29,8 @@
 		private void InitializeComponent()
 		{
 			this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.button1 = new System.Windows.Forms.Button();
 			this.SuspendLayout();
 			// 
 			// webBrowser1
@@ -37,26 +39,49 @@
 			this.webBrowser1.Location = new System.Drawing.Point(0, 0);
 			this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
 			this.webBrowser1.Name = "webBrowser1";
-			this.webBrowser1.Size = new System.Drawing.Size(800, 450);
+			this.webBrowser1.Size = new System.Drawing.Size(817, 492);
 			this.webBrowser1.TabIndex = 0;
 			this.webBrowser1.Url = new System.Uri("https://finance.naver.com/sise/sise_market_sum.nhn", System.UriKind.Absolute);
 			this.webBrowser1.DocumentCompleted += new System.Windows.Forms.WebBrowserDocumentCompletedEventHandler(this.webBrowser1_DocumentCompleted);
+			// 
+			// textBox1
+			// 
+			this.textBox1.Location = new System.Drawing.Point(44, 56);
+			this.textBox1.Name = "textBox1";
+			this.textBox1.Size = new System.Drawing.Size(170, 21);
+			this.textBox1.TabIndex = 1;
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(220, 56);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(61, 23);
+			this.button1.TabIndex = 2;
+			this.button1.Text = "전송";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.button1_Click);
 			// 
 			// Form1
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(800, 450);
+			this.ClientSize = new System.Drawing.Size(817, 492);
+			this.Controls.Add(this.button1);
+			this.Controls.Add(this.textBox1);
 			this.Controls.Add(this.webBrowser1);
 			this.Name = "Form1";
 			this.Text = "Form1";
+			this.Load += new System.EventHandler(this.Form1_Load);
 			this.ResumeLayout(false);
+			this.PerformLayout();
 
 		}
 
 		#endregion
 
 		private System.Windows.Forms.WebBrowser webBrowser1;
+		private System.Windows.Forms.TextBox textBox1;
+		private System.Windows.Forms.Button button1;
 	}
 }
 
--- CSharp/UseWebView/UseWebView/Form1.cs
+++ CSharp/UseWebView/UseWebView/Form1.cs
@@ -7,6 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
+using System.Net.Sockets;
 
 namespace UseWebView
 {
@@ -22,7 +24,47 @@ namespace UseWebView
 			if (webBrowser1.Url.AbsoluteUri != e.Url.AbsoluteUri) return;
 			int asdf = 0;
 			asdf++;
+			//webBrowser1.Document.Encoding
 			//webBrowser1.DocumentText
+			HtmlAgilityPack.HtmlDocument doc = new HtmlAgilityPack.HtmlDocument();
+			Stream docStream = this.webBrowser1.DocumentStream;
+			StreamReader streamReader = new StreamReader(docStream,
+				System.Text.Encoding.GetEncoding(webBrowser1.Document.Encoding));
+			string strHtml = streamReader.ReadToEnd();  // 대충 인코딩하고 strHtml에 담는다는 내용
+
+			//대충 필요한 부분만 잘라내는 내용
+			HtmlAgilityPack.HtmlNode parseNode = doc.DocumentNode.
+				SelectSingleNode("//div[@class='box_type_l']").SelectSingleNode("//table[@class='type_2']");
+			List<List<string>> parsedTbl = parseNode.Descendants("tr")//tr기준으로 자름
+				.Skip(1) //To Skip Table Header Row 헤더 스킵하려고
+				.Where(tr => tr.Elements("td").Count() > 1)// element를 td 기준으로 잘라냄
+				.Select(tr => tr.Elements("td")
+				.Select(td => td.InnerText.Trim())//InnerHtml : 표 안에있는 글자 <-> OuterHtml
+				.ToList()).ToList();
+		}
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+			try
+			{
+				TcpClient tcpClient = new TcpClient();
+				tcpClient.Connect("10.156.146.76", 2101);
+				NetworkStream networkStream = tcpClient.GetStream();
+				MessageBox.Show("연결성공");
+
+				networkStream.Write(Encoding.UTF8.GetBytes("aam jjunggminssssss"), 0, 10);
+				networkStream.Flush();
+				MessageBox.Show("아무튼 성공");
+			}
+			catch
+			{
+				MessageBox.Show("연결실패");
+			}
+		}
+
+		private void Form1_Load(object sender, EventArgs e)
+		{
+
 		}
 	}
 }
